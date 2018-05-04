c92c856b38936d5caeb76ab1f7967f958b725dac
Roman Stankiewicz <roman.stan3@gmail.com>
2018-05-04 23:10:13 +0100

    labels added

--- src/components/Main.js
+++ src/components/Main.js
@@ -14,8 +14,8 @@ import {connect} from 'react-redux'
 class Main extends Component {
 
   state ={
-    title:'',
-    body: '',
+    title:'Success!',
+    body: "You're order has been placed",
     icon:'https://scontent-lhr3-1.xx.fbcdn.net/v/t1.0-1/11083663_10152789631617404_5212765031692940321_n.png?_nc_cat=0&oh=b9b6b907f29cc99e9827eda4d6f4c066&oe=5B5CAB8C'
   }
 
@@ -56,9 +56,12 @@ class Main extends Component {
     return (
       <div id='main'>
         <h3>Spec Ecomm Demo Notifications</h3>
-        <input onChange={this.changeTitle} value={title} type="text" placeholder='Notification Title'/>
-        <input onChange={this.changeBody} value={body} type="text" placeholder='Notification Body'/>
-        <input onChange={this.changeIcon} value={icon} type="text" placeholder='Icon URL'/>
+        <label> Notification Title</label>
+        <input onChange={this.changeTitle} value={title} type="text" />
+        <label> Notification Body</label>
+        <input onChange={this.changeBody} value={body} type="text" />
+        <label> Icon URL</label>
+        <input onChange={this.changeIcon} value={icon} type="text" />
         <div className='button' onClick={this.handlePush}>Push Notification</div>
       </div>
     )
